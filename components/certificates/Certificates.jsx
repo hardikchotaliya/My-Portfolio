c24a98f9d0895cfce6ce7547b1ec7157d1cf2c77
Hardik Chotaliya <14141734+hardikchotaliya@users.noreply.github.com>
2024-12-25 19:45:44 +0530

    Added Cerificate

--- components/certificates/Certificates.jsx
+++ components/certificates/Certificates.jsx
@@ -4,16 +4,16 @@ import HyperOne from '../customH1/HyperOne';
 
 const Certificates = () => {
   const certificates = [
-    { id: 1, name: 'Certificate 1', image: '/imgs/nateg.jpeg' },
-    { id: 2, name: 'Certificate 2', image: '/imgs/enactus.jpeg' },
-    { id: 3, name: 'Certificate 3', image: '/imgs/stress_management.jpeg' },
-    { id: 4, name: 'Certificate 4', image: '/imgs/SixSigmaYellowBelt.jpg' },
-    { id: 5, name: 'Certificate 5', image: '/imgs/50_days_badge.png' },
-    { id: 6, name: 'Certificate 6', image: '/imgs/ScrumforOpsandDevOpsFundamentals.jpg' },
-    { id: 7, name: 'Certificate 7', image: '/imgs/NegotiationAssociate.jpg' },
-    { id: 8, name: 'Certificate 8', image: '/imgs/topreaderbadge.png' },
-    { id: 9, name: 'Certificate 9', image: '/imgs/100_days_badge.png' },
-    { id: 10, name: 'Certificate 10', image: '/imgs/novembertopreader.png' },
+    { id: 1, name: 'Certificate 1', image: '/imgs/istqb-certi.jpg' },
+    { id: 2, name: 'Certificate 2', image: '/imgs/np-appri-certi.jpg' },
+    { id: 3, name: 'Certificate 3', image: '/imgs/psts-certi.jpg' },
+    { id: 4, name: 'Certificate 4', image: '/imgs/genAl-certi.jpeg' },
+    { id: 5, name: 'Certificate 5', image: '/imgs/performTestFound-certi.jpeg' },
+    { id: 6, name: 'Certificate 6', image: '/imgs/testAuto-certi.jpg' },
+    { id: 7, name: 'Certificate 7', image: '/imgs/wdioJS-certi.jpg' },
+    // { id: 8, name: 'Certificate 8', image: '/imgs/topreaderbadge.png' },
+    // { id: 9, name: 'Certificate 9', image: '/imgs/100_days_badge.png' },
+    // { id: 10, name: 'Certificate 10', image: '/imgs/novembertopreader.png' },
   ];
 
   return (
